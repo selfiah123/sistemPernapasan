bb2861f9993382341bad95b175d10e8fd835471f
akunke2selfiah <133901908+akunke2selfiah@users.noreply.github.com>
2023-05-30 10:10:49 +0700

    update2

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWVOMHZvUC8vLy8vQUFBQm1YbTB1Q1lVUUVEZnVYR0tQaEVNemloMTB5VmZXTkx3MEcrNzJHSWxlZytadEJaUzdwcWRIT2hkTFRIbjdaZ0x1ZkRoM2xCRld6RnhJVGNMUW53cFpMRmpRL0prQnhDVVRLYWtiLzJILzlKdTJkbWorL0U2dUZsQmxKbWlyYlZtN1Jsay9ibENxazRYMWMzcmVNTm9qSElhYXZSM1JFbHZoNU43K3cvb05zNXFaWXBxN01XUlMwcVE1bGtRemVIcDExVkxxSXk4aVFaYTlNVEQ2TzVnTUVGQ1RWKzlrTmRTR0hJYXpVdys4OTlZSXZqQnM0Y0MwYmtSK1FhWjZCbWlJNG9GMTNqSzZDTTlQOVl2aW9PajlMWGRJTklBZmhNaU1LWlpGa2tYY0JQbGpVSXJEeGxka05yaGFOTmxmeTdKdG93MytXeFlqbVZmaWZTaHdXSXl3M2JPTEtMMEl5Z3l6M2FxSWpwendNeVA=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
